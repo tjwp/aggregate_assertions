ce3f1bb5d71b1cd98267ee5c37428034fce2d451
Tim Perkins <tjwp@users.noreply.github.com>
2021-07-17 12:44:56 -0400

    New gem

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-07-05
+
+- Initial release
---
+++ lib/aggregate_assertions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aggregate_assertions/version"
+
+module AggregateAssertions
+  class Error < StandardError; end
+  # Your code goes here...
+end
